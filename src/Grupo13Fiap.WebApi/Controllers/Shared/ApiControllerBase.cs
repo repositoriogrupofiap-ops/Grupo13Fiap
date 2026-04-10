83f4010311bc6c9ebd127256182f6140a78bd13c
Guilherme Massi <gmmassi@apoioder.sp.gov.br>
2026-04-10 13:01:46 -0300

    add endpoint para criacao e utilizacao do store e library

--- src/Grupo13Fiap.WebApi/Controllers/Shared/ApiControllerBase.cs
+++ src/Grupo13Fiap.WebApi/Controllers/Shared/ApiControllerBase.cs
@@ -1,10 +1,12 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Grupo13Fiap.WebApi.Controllers.Shared;
-
-// O ideal aqui seria termos um controller base para cada versão e setar o atributo ApiVersion. Ex: v1ControllerBase, v2ControllerBase, etc
-// Isso só não é possível ainda pois a biblioteca de versionamento não suporta isso.
-// Essa feature está em progresso e mapeada para vesão 3: https://github.com/dotnet/aspnet-api-versioning/issues/230
 [ApiController]
 [Route("api/v1")]
-public class ApiControllerBase : ControllerBase { }
+public class ApiControllerBase : ControllerBase
+{
+    protected string? GetIdentityUserId()
+        => (HttpContext.User.Identity as ClaimsIdentity)
+            ?.FindFirst("sub")?.Value;
+}
